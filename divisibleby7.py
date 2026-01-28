a80ab3827f579dc60a4cfef03b13f10f6e5226a5
NIKARA7418 <rajputnirbhaych@gmail.com>
2026-01-28 18:06:31 +0530

    Update codes

--- divisibleby7.py
+++ divisibleby7.py
@@ -3,4 +3,42 @@ if num % 7 == 0:
     print(num, "is divisible by 7")
 else:
     print(num, "is not divisible by 7")
+
+# Write a program to display your name on screen.
+print("Nirbhay Chauhan")
+
+# WAP that will read two numbers and display their sum.
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum = a + b
+print("Sum is:", sum)
+
+# WAP that will read two numbers and display their multiplication.
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+mul = a * b
+print("Multiplication of two numbers is:", mul)
+
+#WAP to read radius of a circle and display its area.
+radius = float(input("Enter radius of circle: "))
+area = 3.14 * radius * radius
+print("Area of circle is:", area)
+
+#WAP that will read principle, rate and time and display simple interest.
+p = int(input("Enter principle amount: "))
+r = int(input("Enter rate of interest: "))
+t = int(input("Enter time in years: "))
+si = (p * r * t ) / 100
+print("Simple Interest is:", si)
+
+#WAP to check if the last digit of a number is 4 or not .
+num = int(input("Enter a number: "))
+if num % 10 == 4:
+    print("Yes Last digit is 4")
+else:
+    print("No Last digit is not 4")
+
+print("nirbhay", end=" ")
+print("chauhan")
+
     
